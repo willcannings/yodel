aa28bf5a053448f8fff94ebf91497c1d85b2e2f5
Will Cannings <me@willcannings.com>
2010-05-20 01:59:12 +1000

    WIP for demo

--- extensions/shop/init.rb
+++ extensions/shop/init.rb
@@ -1,4 +1,4 @@
-#require File.join(File.dirname(__FILE__), 'controllers', 'calendar_controller')
 require File.join(File.dirname(__FILE__), 'models', 'product')
 require File.join(File.dirname(__FILE__), 'models', 'shop_category')
 require File.join(File.dirname(__FILE__), 'models', 'shop_page')
+require File.join(File.dirname(__FILE__), 'controllers', 'cart_controller')

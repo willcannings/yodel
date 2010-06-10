1989e305727b1138bc1e6bd13498fb3a8239604a
Will Cannings <me@willcannings.com>
2010-06-11 01:50:12 +1000

    Change to structure to allow pre-boot config changes; new structure with components and dependencies coming soon

--- core/yodel_application.rb
+++ core/yodel_application.rb
@@ -2,6 +2,8 @@ module Yodel
   class Application < Rack::Builder
     def initialize
       super
+      boot
+      
       use Rack::Reloader, 0
       use Rack::ContentLength
       use Rack::NestedParams
@@ -10,6 +12,56 @@ module Yodel
       Yodel.initialise_middleware_with_app(self)
       run Yodel::RequestHandler.new
       @app = to_app
+    end
+
+    # TODO: extract boot out to "components" with dependencies; automatically resolve boot order based on deps
+    def boot
+      # initialise MongoMapper
+      # FIXME: need port here as well
+      if Yodel.config.database_hostname?
+        MongoMapper.connection = Mongo::Connection.new(Yodel.config.database_hostname)
+      end
+      MongoMapper.database = Yodel.config.database || "Yodel"
+
+      # assign default values if needed
+      Yodel.config.session_secret             ||= "yodel.session"
+      Yodel.config.public_directory_name      ||= "public"
+      Yodel.config.attachment_directory_name  ||= "attachments"
+      
+      # determine root directories
+      Yodel.config.yodel_root = Pathname.new(File.dirname(__FILE__)).join('..')
+      Yodel.config.root = Yodel.config.yodel_root.join('..')
+      Yodel.config.public_directory = Yodel.config.root.join(Yodel.config.public_directory_name)
+
+      # load Yodel components
+      require File.join(File.dirname(__FILE__), 'flash')
+      require File.join(File.dirname(__FILE__), 'mime_types')
+      require File.join(File.dirname(__FILE__), 'middleware')
+      require File.join(File.dirname(__FILE__), 'routes')
+      require File.join(File.dirname(__FILE__), 'controllers')
+      require File.join(File.dirname(__FILE__), 'types')
+      require File.join(File.dirname(__FILE__), 'models')
+      require File.join(File.dirname(__FILE__), 'extensions')
+      
+      # by default, attachments are served from the public folder in the root of the app
+      Yodel.use_middleware do |app|
+        app.use Yodel::ConditionalFile, Yodel.config.public_directory
+      end
+
+      # load yodel and app extensions
+      Yodel.load_extensions(Yodel.config.yodel_root.join('extensions'))
+      Yodel.load_extensions(Yodel.config.root.join('extensions'))
+      Yodel.load_extensions(Yodel.config.root.join('app'))
+
+      # FIXME: hack!!!
+      ObjectSpace.each_object do |obj|
+        if obj.respond_to?(:ancestors) && obj.ancestors.include?(Yodel::AdminController)
+          obj.generate_admin_model_controllers
+        end
+      end
+
+      # deterministic PWD regardless of where we're require'd from
+      Dir.chdir(Yodel.config.root)
     end
     
     def call(env)
--- yodel.rb
+++ yodel.rb
@@ -16,53 +16,6 @@ require 'json'
 require 'rack'
 require 'rack/contrib'
 
-# start app configuration
+# Yodel
 require File.join(File.dirname(__FILE__), 'core', 'config')
-
-# initialise MongoMapper
-# FIXME: need port here as well
-if Yodel.config.database_hostname?
-  MongoMapper.connection = Mongo::Connection.new(Yodel.config.database_hostname)
-end
-MongoMapper.database = Yodel.config.database || "Yodel"
-
-# assign default values if needed
-Yodel.config.session_secret             ||= "yodel.session"
-Yodel.config.public_directory_name      ||= "public"
-Yodel.config.attachment_directory_name  ||= "attachments"
-
-# determine root directories
-Yodel.config.yodel_root = Pathname.new(File.dirname(__FILE__))
-Yodel.config.root = Yodel.config.yodel_root.join('..')
-Yodel.config.public_directory = Yodel.config.root.join(Yodel.config.public_directory_name)
-
-# initialise Yodel
-require File.join(File.dirname(__FILE__), 'core', 'flash')
-require File.join(File.dirname(__FILE__), 'core', 'mime_types')
-require File.join(File.dirname(__FILE__), 'core', 'middleware')
-require File.join(File.dirname(__FILE__), 'core', 'routes')
-require File.join(File.dirname(__FILE__), 'core', 'controllers')
-require File.join(File.dirname(__FILE__), 'core', 'types')
-require File.join(File.dirname(__FILE__), 'core', 'models')
-require File.join(File.dirname(__FILE__), 'core', 'extensions')
-
-# by default, attachments are served from the public folder in the root of the app
-Yodel.use_middleware do |app|
-  app.use Yodel::ConditionalFile, Yodel.config.public_directory
-end
-
-# load yodel and app extensions
-Yodel.load_extensions(Yodel.config.yodel_root.join('extensions'))
-Yodel.load_extensions(Yodel.config.root.join('extensions'))
-Yodel.load_extensions(Yodel.config.root.join('app'))
-
-# FIXME: hack!!!
-ObjectSpace.each_object do |obj|
-  if obj.respond_to?(:ancestors) && obj.ancestors.include?(Yodel::AdminController)
-    obj.generate_admin_model_controllers
-  end
-end
-
-# finally load and start the yodel application
-Dir.chdir(Yodel.config.root)
 require File.join(File.dirname(__FILE__), 'core', 'yodel_application')

51444b1c7853ce2aaae0da27c2989206789b0b9e
Will Cannings <me@willcannings.com>
2010-07-01 17:03:50 +1000

    Requesting an image size which doesn't yet exist automatically creates the new crop size for the image

--- core/models/attachment.rb
+++ core/models/attachment.rb
@@ -73,11 +73,14 @@ module Yodel
   class ImageAttachment < Attachment
     def set_file(file)
       super(file)
-      
-      # crop to the required sizes
+      crop_image
+    end
+    
+    def crop_image
+      puts "CROPPING IMAGES!!!! #{path}"
       sizes = record.associations[attachment_name].options[:sizes]
       return if sizes.nil? || sizes.empty?
-      
+      puts "Creating: #{sizes.inspect}"
       ImageScience.with_image(path.to_s) do |img|
         iw, ih = img.width, img.height
         sizes.each do |name, size|
@@ -88,7 +91,7 @@ module Yodel
           h = [ih, h].min.to_i
           img.with_crop((iw-w)/2, (ih-h)/2, (iw+w)/2, (ih+h)/2) do |crop|
             crop.resize(sw, sh) do |resized|
-              resized.save resized_image_path(name).to_s
+              resized.save resized_image_path(name, false).to_s
             end
           end
         end
@@ -96,12 +99,15 @@ module Yodel
     end
     
     # TODO: shouldn't always be .jpg; have image extension as an option
-    def relative_resized_image_path(name)
-      File.join(relative_directory_path, "#{name}.jpg")
+    def resized_image_path(name, crop_if_required=true)
+      path = Yodel.config.public_directory.join(relative_directory_path, "#{name}.jpg")
+      crop_image unless path.exist? || !crop_if_required
+      path
     end
     
-    def resized_image_path(name)
-      Yodel.config.public_directory.join(relative_resized_image_path(name))
+    # TODO: relative path from is quite a complex method; we should optimise the whole path system here somehow
+    def relative_resized_image_path(name)
+      resized_image_path(name).relative_path_from(Yodel.config.public_directory)
     end
     
     def resized_image_url(name)

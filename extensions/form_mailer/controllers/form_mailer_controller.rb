ac0b5c7f51673d5bad0c4356bf0e77712e519f0b
James <jamsii@gmail.com>
2010-07-24 21:23:11 +1000

    Added XML mimetype

--- extensions/form_mailer/controllers/form_mailer_controller.rb
+++ extensions/form_mailer/controllers/form_mailer_controller.rb
@@ -17,6 +17,10 @@ module Yodel
       @body_prefix ||= prefix
     end
     
+    def self.attachment_1(attachment_1=nil)
+      @attachment_1 ||= attachment_1
+    end    
+    
     def self.default_redirect(path=nil)
       @default_redirect ||= path
     end

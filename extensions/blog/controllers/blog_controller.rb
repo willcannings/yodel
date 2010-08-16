62e4b340b6b7fc4c9203e85a8389b590a9b9312a
Will Cannings <me@willcannings.com>
2010-08-16 17:20:18 +1000

    Fix ordering on blog pages

--- extensions/blog/controllers/blog_controller.rb
+++ extensions/blog/controllers/blog_controller.rb
@@ -6,22 +6,22 @@ module Yodel
     # also need to extract ARTICLES_PER_PAGE
     ARTICLES_PER_PAGE = 5
     def show
-      extra_options = {limit: ARTICLES_PER_PAGE}
+      extra_options = {limit: ARTICLES_PER_PAGE, order: 'published desc'}
       @page_number = params['page'].to_i
       extra_options[:skip] = @page_number * ARTICLES_PER_PAGE      
       
       if params['tag']
         @tag = params['tag']
-        @articles = @page.children.all({tags: @tag}.merge(extra_options)).sort_by{|article| article.published}.reverse
+        @articles = @page.children.all({tags: @tag}.merge(extra_options))
         @total_articles = @page.children.count(tags: @tag)
       elsif params['month'] && params['year']
         @month = [[params['month'].to_i, 1].max, 12].min # constrain the month between 1..12
         @year  = params['year'].to_i
         date_options = {:published.gte => Time.local(@year, @month, 1), :published.lte => Time.local(@year, @month + 1, 1)}
-        @articles = @page.children.all(extra_options.merge(date_options)).sort_by{|article| article.published}.reverse
+        @articles = @page.children.all(extra_options.merge(date_options))
         @total_articles = @page.children.count(date_options)
       else
-        @articles = @page.children.all(extra_options).sort_by{|article| article.published}.reverse
+        @articles = @page.children.all(extra_options)
         @total_articles = @page.children.count
       end
       
